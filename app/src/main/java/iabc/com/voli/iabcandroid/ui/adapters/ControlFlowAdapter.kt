5c683791e096e5450d168747b7cfef4730e647f4
Tengo Meshveliani <meshvelianitengo@gmail.com>
2016-12-16 17:41:34 +0400

    test animacia da home page morcha

---
+++ app/src/main/java/iabc/com/voli/iabcandroid/ui/adapters/ControlFlowAdapter.kt
@@ -0,0 +1,69 @@
+package iabc.com.voli.iabcandroid.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import iabc.com.voli.iabcandroid.R
+import iabc.com.voli.iabcandroid.models.HomeFragmentMovieModel
+
+/**
+ * Created by tengo on 12/16/16.
+ */
+class ControlFlowAdapter(private val context: Context, private val models: List<HomeFragmentMovieModel>) : BaseAdapter(){
+
+    val inflater: LayoutInflater
+
+    init {
+        inflater = LayoutInflater.from(context)
+    }
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var v: View
+
+        if(convertView == null){
+            v = inflater.inflate(R.layout.adapter_item_home_page, null)
+            val locHolder = ViewHolder()
+            locHolder.initFromView(v)
+            v.tag = locHolder
+        }else{
+            v = convertView
+        }
+
+        val holder = v.tag as ViewHolder
+        holder.fillViews(getItem(position))
+        return v
+    }
+
+    override fun getItem(p0: Int): HomeFragmentMovieModel {
+        return models[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return -1.toLong()
+    }
+
+    override fun getCount(): Int {
+        return models.count()
+    }
+
+    inner class ViewHolder{
+        lateinit var imageView: ImageView
+        lateinit var textView: TextView
+
+        fun initFromView(parent: View){
+            imageView = parent.findViewById(R.id.home_fr_moview_adapter_image) as ImageView
+            textView = parent.findViewById(R.id.home_fr_moview_adapter_text) as TextView
+        }
+
+        fun fillViews(item: HomeFragmentMovieModel){
+            Glide.with(context).load(item.image).placeholder(R.mipmap.into_image).into(imageView)
+            textView.text = item.title
+        }
+    }
+}
--- app/src/main/java/iabc/com/voli/iabcandroid/ui/fragments/HomeFragment.kt
+++ app/src/main/java/iabc/com/voli/iabcandroid/ui/fragments/HomeFragment.kt
@@ -9,7 +9,7 @@ import iabc.com.voli.iabcandroid.makeToast
 import iabc.com.voli.iabcandroid.models.HomeFragmentMovieModel
 import iabc.com.voli.iabcandroid.models.HomeFragmentSliderModel
 import iabc.com.voli.iabcandroid.presenter.HomeFragmentPresenter
-import iabc.com.voli.iabcandroid.ui.adapters.HomePageAdapter
+import iabc.com.voli.iabcandroid.ui.adapters.ControlFlowAdapter
 import iabc.com.voli.iabcandroid.ui.adapters.HomePageSliderAdapter
 import iabc.com.voli.iabcandroid.view.HomeFragmentView
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -49,9 +49,9 @@ class HomeFragment : BaseFragment(), HomeFragmentView{
     }
 
     override fun onMoviesResponse(list: List<HomeFragmentMovieModel>) {
-        val adapter = HomePageAdapter(context, list)
-        home_fr_movie_pager.adapter = adapter
-        home_fr_movie_pager.setPageTransformer(true, adapter)
+        val adapter = ControlFlowAdapter(context, list)
+        home_page_control_flow.adapter = adapter
+//        home_fr_movie_pager.setPageTransformer(true, adapter)
 
     }
 
