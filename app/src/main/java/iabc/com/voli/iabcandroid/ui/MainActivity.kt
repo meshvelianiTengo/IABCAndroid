a9d1c9210e0ca1889a5141feab5610e54e691e8e
Tengo Meshveliani <meshvelianitengo@gmail.com>
2016-12-18 20:15:21 +0400

    ragacebi vichaliche

--- app/src/main/java/iabc/com/voli/iabcandroid/ui/MainActivity.kt
+++ app/src/main/java/iabc/com/voli/iabcandroid/ui/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_view, AboutIABCFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.content_main, AboutIABCFragment()).commit()
     }
 }
